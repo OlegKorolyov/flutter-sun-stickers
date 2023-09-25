fa8cc3f247bff158a87807861933b5e597e9721c
okorolyov <o.korolyov@wecandevelopit.com>
2023-09-25 16:25:02 +0300

    cart list design

--- lib/ui/screens/cart_screen.dart
+++ lib/ui/screens/cart_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sun_stickers/data/app_data.dart';
 import 'package:sun_stickers/ui/_ui.dart';
+import 'package:sun_stickers/ui_kit/_ui_kit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -37,20 +39,87 @@ class CartScreenState extends State<CartScreen>{
 
   Widget _cartListView() {
     return ListView.separated(
-        padding: EdgeInsets.all(20),
-        itemCount: 20,
+        padding: EdgeInsets.all(30),
+        itemCount: cartItems.length,
         itemBuilder: (_, index) {
-          return Container(
-            height: 60,
-            width: double.infinity,
-            padding: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.grey
+          final sticker = cartItems[index];
+          return Dismissible(
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) {
+              if (direction == DismissDirection.endToStart) {
+                print('Удаляем');
+              }
+            },
+            key: UniqueKey(),
+            background: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 25,
+                  ),
+                  decoration: BoxDecoration( color: Colors.redAccent,
+                    borderRadius: BorderRadius.circular(15), ),
+                  child: const FaIcon(FontAwesomeIcons.trash), ),
+              ],
             ),
-            child: Text(
-              '${index + 1}',
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            child: Container(
+              height: 80,
+              width: double.infinity,
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const SizedBox(width: 20,),
+                  Image.asset(sticker.image, scale: 10),
+                  const SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        sticker.name,
+                        style:
+                        Theme.of(context).textTheme.displayMedium,
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "\$${sticker.price}",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Column(
+
+                    children: [
+                      CounterButton(
+                        onDecrementTap: () {
+                          print('уменьшили');
+                        },
+                        onIncrementTap: () {
+                          print('увеличили');
+                        },
+                        size: const Size(24, 24),
+                        padding: 0,
+                        label: Text(
+                          sticker.quantity.toString(),
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                      ),
+                      Text(
+                        "\$10",
+                        style: AppTextStyle.h2Style.copyWith(
+                            color: AppColor.accent
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           );
         },
--- lib/ui/widgets/_widgets.dart
+++ lib/ui/widgets/_widgets.dart
@@ -1,2 +1,3 @@
 export 'sticker_list_view.dart';
 export 'empty_wrapper.dart';
+export 'counter_button.dart';
