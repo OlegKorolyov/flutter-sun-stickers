ccd492ab0577774641f2aa5249507d4a4d8acbd8
okorolyov <o.korolyov@wecandevelopit.com>
2023-09-27 16:01:31 +0300

    tab tap

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -14,6 +14,12 @@ class HomeScreenState extends State<HomeScreen> {
   final List<Widget> screens = [const StickerList(), const CartScreen(), const FavoriteScreen(), const ProfileScreen()];
   int currentIndex = 0;
 
+  void onTabTap(int index) {
+    if (currentIndex == index) return;
+    currentIndex = index;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +31,7 @@ class HomeScreenState extends State<HomeScreen> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
-        onTap: (index){},
+        onTap: onTabTap,
         selectedFontSize: 0,
         items: AppData.bottomNavigationItems.map(
             (element) {
